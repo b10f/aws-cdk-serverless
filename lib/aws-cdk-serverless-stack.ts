18a86ccf9149415d1d2a00c69f9032b31566a1c6
Rudolf Bárdos <bardos@outlook.com>
2024-06-29 23:45:36 +0200

    feat: add lambda L2

--- lib/aws-cdk-serverless-stack.ts
+++ lib/aws-cdk-serverless-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// Import Lambda L2 construct
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class AwsCdkServerlessStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkServerlessQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const helloWorldFunction = new lambda.Function(this, 'HelloWorldFunction', {
+            runtime: lambda.Runtime.NODEJS_20_X,
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'index.handler'
+        });
+    }
 }
